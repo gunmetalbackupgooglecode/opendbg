7aec551bc8905d331050f59689876281b64c41ad
d1mk4nah@gmail.com <d1mk4nah@gmail.com@736edd98-e754-0410-bac3-2fa04952274d>
2010-03-20 15:43:30 +0000

    forgotten precompiled header

---
+++ branches/minidbg/src/cli/cli/precompiled.h
@@ -0,0 +1,30 @@
+#ifndef PRECOMPILED_H__
+#define PRECOMPILED_H__
+
+#include "targetver.h"
+#include <cstdio>
+#include <tchar.h>
+#include <windows.h>
+#include <Psapi.h>
+
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <fstream>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/function.hpp>
+#include <boost/signals2.hpp>
+#include <boost/thread.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/thread.hpp>
+#include <boost/ref.hpp>
+#include <boost/bind.hpp>
+#include <boost/program_options.hpp>
+
+#include "udis86.h"
+
+#endif PRECOMPILED_H__ // PRECOMPILED_H_
